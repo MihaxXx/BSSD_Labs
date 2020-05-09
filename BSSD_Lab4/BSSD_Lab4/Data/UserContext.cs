d4d34ad1cad9993028c66b318b9f7810035cc8fd
Mikhail <mikes22@yandex.ru>
2020-05-09 18:05:01 +0300

    Lab 4 User

---
+++ BSSD_Lab4/BSSD_Lab4/Data/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BSSD_Lab4.Models;
+
+namespace BSSD_Lab4.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ BSSD_Lab4/BSSD_Lab4/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BSSD_Lab4.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Secret { get; set; }
+
+        public User()
+        {
+        }
+    }
+}
